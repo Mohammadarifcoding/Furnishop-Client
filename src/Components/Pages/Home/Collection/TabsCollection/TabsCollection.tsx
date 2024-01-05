9551cf2bb07f5fff76e5382f423c6ad1bf902438
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-05 19:26:36 +0600

    Solved the issue with tabs in the collection section

--- src/Components/Pages/Home/Collection/TabsCollection/TabsCollection.tsx
+++ src/Components/Pages/Home/Collection/TabsCollection/TabsCollection.tsx
@@ -3,18 +3,9 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { tabsData } from '../../../../../Model/Model';
 
-import  { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
 
-
-
-// import required modules
-import { Pagination } from 'swiper/modules';
 
 
 
@@ -51,15 +42,15 @@ const TabsCollection = () => {
     return (
         <div className='pb-20'>
              <Tabs>
-    <TabList className='flex overflow-auto flex-nowrap pb-5   text-nowrap  gap-3'>
+    <TabList className='flex overflow-auto flex-nowrap pb-5 tabData md:cursor-default cursor-pointer   text-nowrap  gap-3'>
       {collectionItem.map(item => (
-                    <Tab className='bg-[#00000026] px-4  py-2   rounded-full' key={item.id}>
+                    <Tab className='bg-[#00000026] px-4  py-2 cursor-pointer  rounded-full' key={item.id}>
                         {item.name}
          </Tab>
        ))}
 
     </TabList>
-
+ 
     <TabPanel>
       <h2>Any content 1</h2>
     </TabPanel>
@@ -68,39 +59,7 @@ const TabsCollection = () => {
     </TabPanel>
   </Tabs>
 
-  <Swiper
-        slidesPerView={1}
-        spaceBetween={10}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 50,
-          },
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+  
         </div>
     );
 };
